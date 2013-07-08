8d78a782f10b23fcb4b93574fa157fc77e239612
Martin Hermosilla <mhermosi@gmail.com>
2013-07-08 00:16:36 +0000

    Modified web.js added reference to fs

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
+  var fs = require('fs');
   fs.readFile('index.html', function(err, data) {
       if(err) throw err;
       var buf = new Buffer(data.length);
